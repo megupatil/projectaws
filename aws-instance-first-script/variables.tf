e8ecb4397d5dbb75907a10b520b79450ca8090aa
megupatil <megupatil@gmail.com>
2021-07-17 17:05:22 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -6,8 +6,8 @@ variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    us-east-1    = "ami-0dc2d3e4c0f9ebd18"
+    eu-west-2    = "ami-03ac5a9b225e99b02"
+    eu-central-1 = "ami-00f22f6155d6d92c5"
   }
 }
