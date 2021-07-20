e4af9ba1ebbfad4b85ac7fa0a8ecb5683c4653f0
megupatil <megupatil@gmail.com>
2021-07-20 20:34:21 +0530

    Update variables.tf

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -1,3 +1,6 @@
+variable "AWS_REGION" {    
+    default = "eu-west-2"
+}
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
